96d6d7af71617465b3b1e47e0638c7c56caebe01
DPadillaNYG <DPadilla.NYG@gmail.com>
2018-11-03 17:57:35 -0400

    Updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,27 @@
-// Each game represents  by a string of letters
-var gameOne = ["c", "y", "b", "o", "r", "g"];
+// Each game represented by a string of letters
+var gameOne = ["o", "a", "s", "i", "s"];
 var gameTwo = ["m", "a", "t", "r", "i", "x"];
 var gameThree = ["s", "k", "y", "n", "e", "t"];
 var gameFour = ["r", "e", "k", "a", "l", "l"];
 var gameFive = ["l", "o", "o", "p", "e", "r"];
+var gameSix = ["r", "e", "p", "l", "i", "c", "a", "n", "t", "s"];
+var gameSeven = ["t", "r", "o", "n"];
+var gameEight = ["d", "a", "f", "t", "p", "u", "n", "k"];
+var gameNine = ["r", "o", "b", "o", "c", "o", "p"];
+var gameTen = ["m", "o", "r", "p", "h", "e", "u", "s"];
+var allGames = [gameOne, gameTwo, gameThree, gameFour, 
+                gameFive, gameSix, gameSeven, gameEight, 
+                gameNine, gameTen];
 
-var allGames = [gameOne, gameTwo, gameThree, gameFour, gameFive];
-var gameRandom = allGames[Math.floor(Math.random() * 5)];
+// Audio activated upon first user interaction
+var audio = document.querySelector("audio");
 
 // Global Variables
 var guessAmount = 15;
 var winCount = 0;
-var hasStartedGame = false;
 var letterHistory = [];
 var emptyWord;
+var gameWord;
 
 // Variable references to hold HTML using .textContent
 var underscoreSetup = document.getElementById( "underscore-setup" );
@@ -22,28 +30,28 @@ var letterStat = document.getElementById( "letters-guessed" );
 var winStat = document.getElementById( "win-count" );
 
 winStat.textContent = "WINS: " + winCount;
-underscoreSetup.textContent = "_ _ _ _ _ _";
 letterStat.textContent = "LETTERS GUESSED: " + letterHistory;
 guessStat.textContent = "GUESSES REMAINING: " + guessAmount;
 
-function createEmptyWord(gameWord) {
+function resetWord() {
+    gameWord = allGames[Math.floor(Math.random() * 10)];
     emptyWord = gameWord.slice();
     for ( var i = 0; i < emptyWord.length; i++ ) {
         emptyWord[i] = "_";
     }
+    underscoreSetup.textContent = emptyWord.join(" ");
+} 
+
+function resetStats() {
+    resetWord();
+    guessAmount = 15;
+    guessStat.textContent = "GUESSES REMAINING: " + guessAmount;
+    letterHistory = [];
+    letterStat.textContent = "LETTERS GUESSED: " + letterHistory;
 }
-
-function reloadPage() {
-    window.location.reload();
-}   
-
-var audio = document.querySelector("audio");
-
 
 document.onkeyup = function(event) {
     var userGuess = event.key;
-
-    audio.play();
 
     // Function for depleting guess attempts.
     function userTrys() {
@@ -67,27 +75,25 @@ document.onkeyup = function(event) {
         }
     }
 
-    userTrys();
-
-    function userProgress(gameWord) {   
+    function userProgress() {   
         for ( var i = 0; i < gameWord.length; i++ ) {
             if ( userGuess === gameWord[i] ) {
                 if ( guessAmount > 0 ) {
                     emptyWord[i] = gameWord[i];
+                } else {
+                    resetStats();
                 }
             }
         }
         if (emptyWord.join() === gameWord.join()) {
             winCount++;
             winStat.textContent = "WINS: " + winCount;
-            
+            resetStats();
         }
         underscoreSetup.textContent = emptyWord.join(" ");
     }
 
-    userProgress(gameRandom);
-
-    function letterStats(gameWord) {
+    function lettersUsed() {
         if ( userGuess ) {
             if ( letterHistory.indexOf(userGuess) === -1 ) {
                 if ( userGuess === "a" || userGuess === "b" || userGuess === "c" ||
@@ -99,22 +105,20 @@ document.onkeyup = function(event) {
                      userGuess === "s" || userGuess === "t" || userGuess === "u" ||
                      userGuess === "v" || userGuess === "w" || userGuess === "x" ||
                      userGuess === "y" || userGuess === "z" ) {
-                    if ( guessAmount > 0 ) {
-                        letterHistory.push(userGuess);
-                        letterStat.textContent = "LETTERS GUESSED: " + letterHistory.join(", ");
-                    }
+                    letterHistory.push(userGuess);
+                    letterStat.textContent = "LETTERS GUESSED: " + letterHistory.join(", ");
                 }
             }
         }
     }
 
-    letterStats(gameRandom);
+    audio.play();
+    userTrys();
+    lettersUsed();
+    userProgress();
 
 }
 
-createEmptyWord(gameRandom);
-
-// if guess amount reaches zero, end game
-// After a win or loss, a new game will appear to play with new word.
+resetWord();
 
 
