f3e327cb1e4c443a1768bef5afc125e10d8000fd
DPadillaNYG <DPadilla.NYG@gmail.com>
2018-11-04 05:34:28 -0500

    Final Revision

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,5 @@
+// For the sake of grading and understanding the logic, I have attached comments below:
+
 // Each game represented by a string of letters
 var gameOne = ["o", "a", "s", "i", "s"];
 var gameTwo = ["m", "a", "t", "r", "i", "x"];
@@ -9,39 +11,47 @@ var gameSeven = ["t", "r", "o", "n"];
 var gameEight = ["d", "a", "f", "t", "p", "u", "n", "k"];
 var gameNine = ["r", "o", "b", "o", "c", "o", "p"];
 var gameTen = ["m", "o", "r", "p", "h", "e", "u", "s"];
-var allGames = [gameOne, gameTwo, gameThree, gameFour, 
-                gameFive, gameSix, gameSeven, gameEight, 
-                gameNine, gameTen];
+var allGames = [ gameOne, gameTwo, gameThree, gameFour, 
+                 gameFive, gameSix, gameSeven, gameEight, 
+                 gameNine, gameTen ];
 
-// Audio activated upon first user interaction
-var audio = document.querySelector("audio");
+// Audio activated upon first user interaction by "click"
+var cyberMusic = document.getElementById( "cybermusic" );
+var cityRain = document.getElementById( "cityrain" );
 
 // Global Variables
-var guessAmount = 15;
-var winCount = 0;
-var letterHistory = [];
-var emptyWord;
-var gameWord;
+var guessAmount = 15; // User record of guesses left
+var winCount = 0; // User record of wins
+var letterHistory = []; // Letters guessed get pushed into this empty array
+var gameWord; // For storing randomized word
+var emptyWord; // For storing a duplicate of gameWord to manipulate in-game
+var isPlaying = true; // Flag variable created to block user input at win/loss alert box
+var isAnimated = true; // Flag variable to get png image to animate indefinitely
 
-// Variable references to hold HTML using .textContent
+// Variable references to hold HTML elements (refer to children of <main> in index.html)
 var underscoreSetup = document.getElementById( "underscore-setup" );
 var guessStat = document.getElementById( "guess-pool" );
 var letterStat = document.getElementById( "letters-guessed" );
 var winStat = document.getElementById( "win-count" );
 
+// Initial HTML write-ins
 winStat.textContent = "WINS: " + winCount;
 letterStat.textContent = "LETTERS GUESSED: " + letterHistory;
 guessStat.textContent = "GUESSES REMAINING: " + guessAmount;
 
+// This function randomizes the word being played
 function resetWord() {
-    gameWord = allGames[Math.floor(Math.random() * 10)];
+    gameWord = allGames[ Math.floor( Math.random() * 10 ) ];
     emptyWord = gameWord.slice();
+
     for ( var i = 0; i < emptyWord.length; i++ ) {
         emptyWord[i] = "_";
     }
+
     underscoreSetup.textContent = emptyWord.join(" ");
 } 
 
+// This function reinitializes the user stats & word for the next game
 function resetStats() {
     resetWord();
     guessAmount = 15;
@@ -50,51 +60,35 @@ function resetStats() {
     letterStat.textContent = "LETTERS GUESSED: " + letterHistory;
 }
 
-document.onkeyup = function(event) {
+// This function ends the game when the user has lost ( kinda "hacky" with UserProgress() function )
+function losingGame() {
+    if ( guessAmount === 0 ) {
+        isPlaying = false;
+        swal( '"The answer was ' + gameWord.join("").toUpperCase() +
+        '. All those letters will be lost in time...like tears in the rain."' )
+            .then( function() {
+                isPlaying = true;
+            });
+        resetStats();
+    }
+}
+
+document.onkeyup = function( event ) {
+    // Establishing user input
     var userGuess = event.key;
 
-    // Function for depleting guess attempts.
+    // Converts image into gif indefinitely using a flag variable upon user click
+    function solidifyGif() {
+        if ( isAnimated ) {
+            isAnimated = false;
+            document.getElementById("cyber-city").src="assets/images/CyberPunk.gif";
+        }
+    }
+
+    // Depletes guess attempts ( alphabet-check to ignore other key strokes )
     function userTrys() {
-        if ( userGuess ) {
-            if ( letterHistory.indexOf(userGuess) === -1 ) {
-                if ( userGuess === "a" || userGuess === "b" || userGuess === "c" ||
-                     userGuess === "d" || userGuess === "e" || userGuess === "f" ||
-                     userGuess === "g" || userGuess === "h" || userGuess === "i" ||
-                     userGuess === "j" || userGuess === "k" || userGuess === "l" ||
-                     userGuess === "m" || userGuess === "n" || userGuess === "o" ||
-                     userGuess === "p" || userGuess === "q" || userGuess === "r" ||
-                     userGuess === "s" || userGuess === "t" || userGuess === "u" ||
-                     userGuess === "v" || userGuess === "w" || userGuess === "x" ||
-                     userGuess === "y" || userGuess === "z") {
-                    if ( guessAmount > 0 ) {
-                        guessAmount--;
-                        guessStat.textContent = "GUESSES REMAINING: " + guessAmount;
-                    }
-                }
-            }
-        }
-    }
-
-    function userProgress() {   
-        for ( var i = 0; i < gameWord.length; i++ ) {
-            if ( userGuess === gameWord[i] ) {
-                if ( guessAmount > -1 ) {
-                    emptyWord[i] = gameWord[i];
-                }
-            }
-        }
-        if (emptyWord.join() === gameWord.join()) {
-            winCount++;
-            winStat.textContent = "WINS: " + winCount;
-            swal('"As you no doubt have guessed I am ' + gameWord.join("").toUpperCase() + '"');
-            resetStats();
-        }
-        underscoreSetup.textContent = emptyWord.join(" ");
-    }
-
-    function lettersUsed() {
-        if ( userGuess ) {
-            if ( letterHistory.indexOf(userGuess) === -1 ) {
+        if ( userGuess && isPlaying ) {
+            if ( letterHistory.indexOf( userGuess ) === -1 ) {
                 if ( userGuess === "a" || userGuess === "b" || userGuess === "c" ||
                      userGuess === "d" || userGuess === "e" || userGuess === "f" ||
                      userGuess === "g" || userGuess === "h" || userGuess === "i" ||
@@ -104,24 +98,68 @@ document.onkeyup = function(event) {
                      userGuess === "s" || userGuess === "t" || userGuess === "u" ||
                      userGuess === "v" || userGuess === "w" || userGuess === "x" ||
                      userGuess === "y" || userGuess === "z" ) {
-                    letterHistory.push(userGuess);
-                    letterStat.textContent = "LETTERS GUESSED: " + letterHistory.join(", ");
+                    if ( guessAmount > 0 ) {
+                        guessAmount--;
+                        guessStat.textContent = "GUESSES REMAINING: " + guessAmount;
+                    }
                 }
             }
         }
     }
 
-    audio.play();
+    // Tracks user input while refreshing user progress and also checks for win conditions
+    function userProgress() {   
+        for ( var i = 0; i < gameWord.length; i++ ) {
+            if ( userGuess === gameWord[i] && isPlaying ) {
+                if ( guessAmount > -1 ) {
+                    emptyWord[i] = gameWord[i];
+                }
+            }
+        }
+
+        if ( emptyWord.join() === gameWord.join() ) {
+            winCount++;
+            winStat.textContent = "WINS: " + winCount;
+            isPlaying = false;
+            swal( '"As you no doubt have guessed I am ' + gameWord.join("").toUpperCase() 
+                 + '"' ).then( function() {
+                            isPlaying = true;
+                        });
+            resetStats();
+        }
+
+        underscoreSetup.textContent = emptyWord.join(" ");
+    }
+
+    // Keeps track of letters guessed ( alphabet-check to ignore other key strokes )
+    function lettersUsed() {
+        if ( userGuess && isPlaying ) {
+            if ( letterHistory.indexOf( userGuess ) === -1 ) {
+                if ( userGuess === "a" || userGuess === "b" || userGuess === "c" ||
+                     userGuess === "d" || userGuess === "e" || userGuess === "f" ||
+                     userGuess === "g" || userGuess === "h" || userGuess === "i" ||
+                     userGuess === "j" || userGuess === "k" || userGuess === "l" ||
+                     userGuess === "m" || userGuess === "n" || userGuess === "o" ||
+                     userGuess === "p" || userGuess === "q" || userGuess === "r" ||
+                     userGuess === "s" || userGuess === "t" || userGuess === "u" ||
+                     userGuess === "v" || userGuess === "w" || userGuess === "x" ||
+                     userGuess === "y" || userGuess === "z" ) {
+                    letterHistory.push( userGuess );
+                    letterStat.textContent = "LETTERS GUESSED: " + letterHistory.sort().join(", ");
+                }
+            }
+        }
+    }
+
+    // Function calls! 
+    cyberMusic.play();
+    cityRain.play();
+    solidifyGif();
     userTrys();
     lettersUsed();
     userProgress();
+    losingGame();
 
 }
 
 resetWord();
-
-// swal('"The answer was ' + gameWord.join("").toUpperCase() +
-// '. All those letters will be lost in time...like tears in the rain."');
-// resetStats();
-
-
